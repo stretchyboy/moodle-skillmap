0f1aa48969361b3c8cca70feb9ae9410920a7f53
martyn.eggleton <martyn.eggleton@d08329a0-abce-11de-b6f4-e1be98fab775>
2009-10-20 13:14:40 +0000

    Adds lang files

---
+++ mod/resource/type/taggedfile/lang/en_utf8/taggedfile.php
@@ -0,0 +1,5 @@
+<?PHP // $Id: skillmap.php,v 1.6.4.3 2008/01/18 00:39:01 danmarsden Exp $
+      // skillmap.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['resourcetypetaggedfile'] = 'Link to a file or web site with tagging';
+?>
