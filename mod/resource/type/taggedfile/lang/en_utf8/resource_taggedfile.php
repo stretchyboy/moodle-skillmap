d1fec27f73a56c3e3c3cf73e4d4dd89a658141c2
martyn.eggleton <martyn.eggleton@d08329a0-abce-11de-b6f4-e1be98fab775>
2009-11-09 10:43:52 +0000

    Adds language files for the resource_taggedfile type and the block that displays them

---
+++ mod/resource/type/taggedfile/lang/en_utf8/resource_taggedfile.php
@@ -0,0 +1,16 @@
+<?PHP // $Id: skillmap.php,v 1.6.4.3 2008/01/18 00:39:01 danmarsden Exp $
+      // skillmap.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['resourcetypetaggedfile'] = 'Link to a file or web site with tagging';
+
+
+
+$string['blocktagstitle'] = 'Tagged Resources';
+$string['numberofentries'] = 'Number of Resources';
+$string['tagtext'] = 'Tag';
+$string['tagdatelastused'] = 'Tag Last Used';
+
+$string['modulename'] = 'Tagged Resource';
+$string['modulenameplural'] = 'Tagged Resources';
+
+?>
